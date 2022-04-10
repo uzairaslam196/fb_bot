912438491997fbc68ee5844f2614a85a150c4ba2
Uzair Aslam <uzair.aslam@invozone.com>
2022-04-10 22:48:24 +0500

    Impl EPs to handle webhook events

--- lib/fb_bot_3_web/controllers/webhook_controller.ex
+++ lib/fb_bot_3_web/controllers/webhook_controller.ex
@@ -1,17 +1,39 @@
 defmodule FbBot3Web.WebhookController do
-use FbBot3Web, :controller
+  use FbBot3Web, :controller
+  require Logger
+
+  @verification_key Application.get_env(:fb_bot_3, :verification_key)
 
   def challenge(conn, params) do
-    challenge = params["hub.challenge"] && params["hub.challenge"] |> String.to_integer()
-    IO.inspect(conn)
-    IO.inspect(params)
-    IO.inspect("hereeee")
-    json(conn, challenge)
+    if params["hub.verify"] == @verification_key do
+      challenge = params["hub.challenge"] && String.to_integer(params["hub.challenge"])
+      json(conn, challenge)
+    else
+      conn
+    end
   end
 
   def message_events(conn, params) do
-    IO.inspect(conn)
-    IO.inspect(params)
-    json(conn, 123)
+    FbBot3.Messages.process(params)
+    [message | _] = params["entry"]["messaging"]
+
+    case do_message_events(message) do
+      {:ok, _} ->
+        Logger.info("Successfully sent the message's response")
+      {:error, message} ->
+        Logger.error(message)
+    end
+
+    conn
   end
+
+  defp do_message_events(%{"message" => _message, "recipient" => %{"id" => id}}) do
+    FbBot3.Messages.process(id)
+  end
+
+  defp do_message_events(%{"postback" => postback, "recipient" => %{"id" => id}}) do
+    FbBot3.Messages.process(postback, id)
+  end
+
+  defp do_message_events(_), do: nil
 end
