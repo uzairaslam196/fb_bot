d7535c5ada257b478597c717896c6b469417ec53
Uzair Aslam <uzair.aslam@invozone.com>
2022-04-11 00:10:13 +0500

    Avoid plug error

--- lib/fb_bot_3_web/controllers/webhook_controller.ex
+++ lib/fb_bot_3_web/controllers/webhook_controller.ex
@@ -8,7 +8,7 @@ defmodule FbBot3Web.WebhookController do
     IO.inspect(conn)
     IO.inspect("params")
     IO.inspect(params)
-    if params["hub.verify"] == @verification_key do
+    if params["hub.verify_token"] == @verification_key do
       challenge = params["hub.challenge"] && String.to_integer(params["hub.challenge"])
       json(conn, challenge)
     else
@@ -24,12 +24,11 @@ defmodule FbBot3Web.WebhookController do
       {:ok, _} ->
         Logger.info("Successfully sent the message's response")
         json(conn, "Successfully sent")
+
       {:error, message} ->
         Logger.error(message)
         json(conn, message)
     end
-
-
   end
 
   defp do_message_events(%{"message" => _message, "recipient" => %{"id" => id}}) do
